69c5ca4bb3f64bc96ce507405bebe1e58f3d5ce5
PRATEEK SATI <prateeksati@PRATEEKs-MacBook-Pro.local>
2023-03-27 15:58:33 +0530

    added a space to track in git

--- server/controller/conversation-controller.js
+++ server/controller/conversation-controller.js
@@ -1,6 +1,7 @@
 import Conversation  from "../modal/Conversation.js";
 
 
+
 export const newConversation = async (request, response) => {
     let senderId = request.body.senderId;
     let receiverId = request.body.receiverId;
